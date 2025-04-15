99eec5c6af6e07459aea0656b7ab901de0275af5
Matthew Law <matthew.law@standardbank.co.za>
2025-04-15 11:47:51 +0200

    Solution Structure

--- Simple.Auth/Builders/AuthenticationOptionsBuilder.cs
+++ Simple.Auth/Builders/AuthenticationOptionsBuilder.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+ 
 namespace Simple.Auth.Builders
 {
     public class AuthenticationOptionsBuilder
